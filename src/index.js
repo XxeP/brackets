79833633aae51de4db106143a6c1f852841846c9
XxeP <neg-nazarov@rambler.ru>
2021-03-08 15:18:55 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var array = []
+        temp = str
+    for(var i = 0; i < bracketsConfig.length; i++) {
+        array[bracketsConfig[i][1]] = bracketsConfig[i].join('');
+    }
+
+    for (var j = 0; j < str.length; j++) {
+        if(array[str[j]] != undefined) {
+            temp = temp.replace(array[str[j]], '');
+       }
+    }
+    
+    if (temp.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
